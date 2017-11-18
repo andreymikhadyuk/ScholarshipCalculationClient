7730a9f04f9f1ba8a2a81289f50809fbaba04544
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2017-11-18 13:50:07 +0300

    Service for login/registration was created

---
+++ src/main/java/com/mikhadyuk/scholarshipcalculator/service/EntranceService.java
@@ -0,0 +1,43 @@
+package com.mikhadyuk.scholarshipcalculator.service;
+
+import com.mikhadyuk.scholarshipcalculator.connection.ActionType;
+import com.mikhadyuk.scholarshipcalculator.connection.ServerConnection;
+import com.mikhadyuk.scholarshipcalculator.model.User;
+import com.mikhadyuk.scholarshipcalculator.model.enums.Role;
+
+import java.io.IOException;
+
+public class EntranceService {
+    private ServerConnection serverConnection;
+
+    private static EntranceService instance;
+
+    private EntranceService() {
+        serverConnection = ServerConnection.getInstance();
+    }
+
+    public static EntranceService getInstance() {
+        if (instance == null) {
+            instance = new EntranceService();
+        }
+        return instance;
+    }
+
+    public void register(String username, String password, Role role,
+                         String lastName, String firstName, String patronymic) {
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(password);
+        user.setRole(role);
+        user.setLastName(lastName);
+        user.setFirstName(firstName);
+        user.setPatronymic(patronymic);
+        try {
+            serverConnection.send(ActionType.REGISTRATION);
+            serverConnection.send(user);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+}
